d12793a29e42a79cafa44292f49f6a73ccc3da92
zhangyi <yzhanget@connect.ust.hk>
2018-10-23 00:39:14 +0800

    typo fix

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_required
-from app import db, sockets
+from app import db, sockets, log
 from app.main.forms import EditProfileForm
 from app.models import User
 from app.main import bp
